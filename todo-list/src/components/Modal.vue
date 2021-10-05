d82c738edad1a2809691845b40f9be5f0c4911a4
yoyo <yoyointheho@gmail.com>
2021-10-05 11:59:58 +0900

    Created basic modal

---
+++ todo-list/src/components/Modal.vue
@@ -0,0 +1,15 @@
+<template>
+	<form>
+		<label for="input">What do you have to do</label>
+		<br />
+		<input type="text" name="input" id="input" />
+		<br />
+		<input type="submit" value="submit" id="submit" />
+	</form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
